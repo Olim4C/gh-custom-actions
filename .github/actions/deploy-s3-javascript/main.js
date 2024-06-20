4875cd6033e6d54d922df5b4fc0eb180880b8955
Camilo Ponce <camilo.ponce92@outlook.com>
2024-06-19 22:49:35 -0400

    feat: finish javascript custom action to deploy to a S3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,15 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
+    // 1) Get some inputs values
+    const bucket = core.getInput('bucket', { require: true })
+    const bucketRegion = core.getInput('bucket-region', { require: true })
+    const distFolder = core.getInput('dist-forlder', { require: true })
+
+    // 2) Upload files
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+    
     core.notice('Hello from my custom JavaScript Action!');
 }
 
